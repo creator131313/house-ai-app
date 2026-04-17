c4fad5063548d9f81551ec50d58637e2a85645ba
creator131313 <sjuneja.5031@gmail.com>
2026-04-17 22:34:08 +0530

    Update app.py
    Updated port

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def home():
     return render_template_string(HTML, result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
